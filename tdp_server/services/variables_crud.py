da2f83a24482c7e6c3c694eaa6ee9952c90cb28f
Guillaume Boutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-04-11 13:16:51 +0200

    services: remove GitRepository from typing

--- tdp_server/services/variables_crud.py
+++ tdp_server/services/variables_crud.py
@@ -1,6 +1,5 @@
 from typing import Any, Mapping, Optional, Tuple
 
-from tdp.core.repository.git_repository import GitRepository
 from tdp.core.service_manager import ServiceManager
 from tdp_server.schemas import Variables
 
@@ -11,7 +10,7 @@ class VariablesCrud:
         service_manager: ServiceManager, name: Optional[str] = None
     ) -> Variables:
         filename: str = name or service_manager.name
-        repository: GitRepository = service_manager.repository
+        repository = service_manager.repository
         with repository.open_var_file(filename + ".yml") as configuration:
             return Variables(__root__=configuration._content)
 
@@ -26,7 +25,7 @@ class VariablesCrud:
         filename: str = name or service_manager.name
 
         update_message = f"[{service_manager.name}] {message}"
-        repository: GitRepository = service_manager.repository
+        repository = service_manager.repository
         with repository.validate(update_message) as repo, repo.open_var_file(
             f"{filename}.yml"
         ) as service_variables:
