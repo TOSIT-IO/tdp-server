e04d021ab89fa2948bdbfd766c7cb6637f3a4f76
Guillaume Boutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-05-02 11:02:25 +0200

    tdp-server: add tdp vars initialization script

---
+++ tdp_server/initialize_tdp_vars.py
@@ -0,0 +1,7 @@
+from tdp.core.dag import Dag
+from tdp.core.service_manager import ServiceManager
+from tdp_server.core.config import settings
+
+ServiceManager.initialize_service_managers(
+    Dag(), settings.TDP_VARS, settings.TDP_COLLECTION_PATH / "tdp_vars_defaults"
+)
