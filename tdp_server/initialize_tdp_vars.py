193c2dcd020fed9c9e7e0fd9fdd0d0c8fe398b5a
Guillaume Boutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-05-03 15:18:21 +0200

    endpoints: update dag creation in dependencies

--- tdp_server/initialize_tdp_vars.py
+++ tdp_server/initialize_tdp_vars.py
@@ -1,7 +1,10 @@
 from tdp.core.dag import Dag
 from tdp.core.service_manager import ServiceManager
+
 from tdp_server.core.config import settings
 
 ServiceManager.initialize_service_managers(
-    Dag(), settings.TDP_VARS, settings.TDP_COLLECTION_PATH / "tdp_vars_defaults"
+    Dag.from_collection(settings.TDP_COLLECTION_PATH),
+    settings.TDP_VARS,
+    settings.TDP_COLLECTION_PATH / "tdp_vars_defaults",
 )
