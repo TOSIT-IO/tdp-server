36fa7073fdd985a134bad85fb53c5aac92f3e629
Guillaume Boutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-06-20 15:54:45 +0200

    feat: add deployment information endpoint

--- tdp_server/schemas/deployment.py
+++ tdp_server/schemas/deployment.py
@@ -1,6 +1,10 @@
+from datetime import datetime
 from typing import List, Optional
 
 from pydantic import BaseModel
+from tdp.core.runner.executor import StateEnum
+
+from tdp_server.schemas.operation import Operation
 
 
 class DeployRequest(BaseModel):
@@ -11,3 +15,18 @@ class DeployRequest(BaseModel):
 
 class DeployStatus(BaseModel):
     message: str
+
+
+class Deployment(BaseModel):
+    id: int
+    sources: List[str] = []
+    targets: List[str] = []
+    filter: str
+    start: datetime
+    end: datetime
+    state: StateEnum
+    operations: List[str]
+
+
+class DeploymentWithOperations(Deployment):
+    operations: List[Operation]
