873c3aea991c6f1bf593c532905929fecbf5a2e8
Guillaume Boutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-06-21 11:59:33 +0200

    fix: set router prefix to /service/{service_id}/component

--- tdp_server/api/v1/api.py
+++ tdp_server/api/v1/api.py
@@ -5,5 +5,7 @@ from tdp_server.api.v1.endpoints import component, deploy, service
 api_router = APIRouter()
 
 api_router.include_router(service.router, prefix="/service", tags=["services"])
-api_router.include_router(component.router, prefix="/service", tags=["components"])
+api_router.include_router(
+    component.router, prefix="/service/{service_id}/component", tags=["components"]
+)
 api_router.include_router(deploy.router, prefix="/deploy", tags=["deploy"])
