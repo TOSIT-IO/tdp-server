05bbb8c5cb46ec1561aa6fff31819f0dbf5476e6
gboutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-10-05 16:00:10 +0200

    fix: add tilde expantion to path validation

---
+++ tdp_server/core/validator_types.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from typing import Any, Dict
+
+from pydantic import DirectoryPath
+
+
+class ExistingDir(DirectoryPath):
+    @classmethod
+    def __get_validators__(cls) -> "CallableGenerator":  # type: ignore
+        yield cls.validate_expanduser
+        yield from DirectoryPath.__get_validators__()  # type: ignore
+
+    @classmethod
+    def validate_expanduser(cls, value: Path) -> Path:
+        return Path(value).expanduser().resolve()
