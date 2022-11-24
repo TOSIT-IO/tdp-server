acdf3a2ba0e7b9277b780b5be11f14956d009522
gboutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-11-24 10:36:39 +0100

    docs: document DeployRequest parameters

--- tdp_server/schemas/deployment.py
+++ tdp_server/schemas/deployment.py
@@ -1,19 +1,41 @@
 from datetime import datetime
-from re import compile
 from typing import List, Optional
 
-from pydantic import BaseModel, validator
-from tdp.core.models import DeploymentLog, FilterTypeEnum, StateEnum
+from pydantic import BaseModel, Field, validator
+from tdp.core.models import FilterTypeEnum, StateEnum
 
 from tdp_server.schemas.operation import Operation
 
 
 class DeployRequest(BaseModel):
-    targets: Optional[List[str]] = None
-    sources: Optional[List[str]] = None
-    filter_type: Optional[FilterTypeEnum] = None
-    filter_expression: Optional[str] = None
-    restart: bool = False
+    targets: Optional[List[str]] = Field(
+        default=None,
+        title="Deployment Targets",
+        description="List of operations used as targets on the dag generation, mutually exclusive with `sources`.",
+    )
+    sources: Optional[List[str]] = Field(
+        default=None,
+        title="Deployment Sources",
+        description="List of operations used as sources on the dag generation, mutually exclusive with `targets`.",
+    )
+    filter_type: Optional[FilterTypeEnum] = Field(
+        default=None,
+        title="Deployment Sources",
+        description="Controls how the filter expression must be interpreted.",
+    )
+    filter_expression: Optional[str] = Field(
+        default=None,
+        title="Filter Expression",
+        description=(
+            "Expression which will match on the operation list generated from the dag. "
+            "Only operations matching will be kept for the deployment."
+        ),
+    )
+    restart: bool = Field(
+        default=False,
+        title="Replace starts",
+        description="Controls whether or not start operations will be replaced by a restart operation.",
+    )
 
     @validator("sources")
     def sources_validator(cls, value, values):
