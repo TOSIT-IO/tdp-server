a1ff96c5661c979fdaaeadb49f16ecbe8d02b518
gboutry <diode-boutry.consultant@dgfip.finances.gouv.fr>
2022-12-05 11:09:47 +0100

    refactor: use pydantic facilities to parse models

--- tdp_server/services/utils.py
+++ tdp_server/services/utils.py
@@ -1,37 +1,25 @@
-from datetime import datetime, timezone
-from typing import Optional
+from typing import Type, TypeVar
 
 from pydantic import parse_obj_as
 from tdp.core.models import DeploymentLog
 from tdp.core.operation import Operation as tdp_Operation
+from tdp.core.models import DeploymentLog as tdp_DeploymentLog
 
 from tdp_server.schemas import DeploymentLog, Operation
 
-
-def to_utc_datetime(dt: datetime) -> datetime:
-    return dt.replace(tzinfo=timezone.utc)
-
-
-def to_optional_utc_datetime(dt: Optional[datetime]) -> Optional[datetime]:
-    if dt is not None:
-        to_utc_datetime(dt)
-    return dt
-
-
-def deployment_from_deployment_log(deployment_log: DeploymentLog, user: str):
-    return DeploymentLog(
-        id=deployment_log.id,  # type: ignore
-        sources=deployment_log.sources,  # type: ignore
-        targets=deployment_log.targets,  # type: ignore
-        filter_expression=deployment_log.filter_expression,  # type: ignore
-        start_time=to_utc_datetime(deployment_log.start_time),  # type: ignore
-        end_time=to_optional_utc_datetime(deployment_log.end_time),  # type: ignore
-        restart=deployment_log.restart,  # type: ignore
-        state=deployment_log.state,  # type: ignore
-        operations=list(map(lambda op: op.operation, deployment_log.operations)),
-        user=user,
-    )
+D = TypeVar("D", bound=DeploymentLog)
 
 
 def operation_schema_from_operation(operation: tdp_Operation) -> Operation:
     return parse_obj_as(Operation, operation.__dict__)
+
+
+def parse_deployment_log(
+    deployment_log: tdp_DeploymentLog, schema_class: Type[D], user=None
+) -> D:
+    deployment_schema = schema_class.from_orm(deployment_log)
+    if user is not None:
+        deployment_schema.user = user
+    if deployment_log.user_deployment_log is not None:
+        deployment_schema.user = deployment_log.user_deployment_log.user_identifier
+    return deployment_schema
