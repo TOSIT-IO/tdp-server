0f5a2ee68adb5194e0833af547b428b73cde651d
gboutry <boutryguillaume1@gmail.com>
2022-12-15 10:18:25 +0100

    refactor: normalize tdp user

--- dev/config/init_script.sql
+++ dev/config/init_script.sql
@@ -1,20 +1,18 @@
 CREATE USER keycloak WITH PASSWORD 'keycloak';
-CREATE USER tdp_user WITH PASSWORD 'tdp_password';
+CREATE USER tdp WITH PASSWORD 'tdp';
 ---
 CREATE DATABASE keycloak;
 \connect keycloak
 CREATE SCHEMA keycloak AUTHORIZATION keycloak;
 \connect postgres
 
-CREATE DATABASE tdp_dev_db;
-\connect tdp_dev_db
-CREATE SCHEMA tdp AUTHORIZATION tdp_user;
+CREATE DATABASE tdp;
+\connect tdp
+CREATE SCHEMA tdp AUTHORIZATION tdp;
 \connect postgres
 ---
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA keycloak TO keycloak;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA keycloak TO keycloak;
 
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA tdp TO tdp_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA tdp TO tdp_user;
--- set default schema for tdp user
-ALTER ROLE tdp_user SET search_path TO tdp,public;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA tdp TO tdp;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA tdp TO tdp;
