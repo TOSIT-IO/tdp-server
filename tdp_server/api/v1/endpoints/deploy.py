ce84d4e8e5027822b0cc01d33f612d27bb625d84
SteBaum <stephan.baum@edu.dsti.institute>
2023-12-08 09:55:52 +0100

    feat: added commentary for deploy endpoint

--- tdp_server/api/v1/endpoints/deploy.py
+++ tdp_server/api/v1/endpoints/deploy.py
@@ -18,4 +18,25 @@ router = APIRouter()
     },
 )
 def deploy(options: DeployOptions):
+    """
+    Deploys the planned dag/operations.
+
+    Options:
+
+        - force_stale_update: Force stale status update.
+
+        - dry: Execute the dag without running any actions.
+
+        - mock_deploy: Mock the deploy, do not actually run the ansible playbook.
+
+        - run_directory: Working directory where the executor is launched (`ansible-playbook` for Ansible).
+
+        - validate: validates service variables.
+
+        - collection: Path to the collections.
+
+        - database_dsn: Database Data Square Name, in sqlalchemy driver for example: sqlite:////data/tdp.db.
+
+        - vars: Path to the TDP variables.
+    """
     pass
