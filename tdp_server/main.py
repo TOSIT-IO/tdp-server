bc290becb11e9dd335e1e7d52730d8085aad5d9a
SteBaum <stephan.baum@edu.dsti.institute>
2023-12-19 19:19:29 +0100

    feat: added an endpoint which list all other endpoints with get method

--- tdp_server/main.py
+++ tdp_server/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from fastapi_pagination import add_pagination
+from typing import List
+
 from tdp_server.api.v1.api import api_router
 
 
@@ -11,3 +13,24 @@ add_pagination(app)
 @app.get("/")
 async def root():
     return {"tdp-server"}
+
+
+def get_all_get_endpoints() -> List[str]:
+    """
+    Returns a list of all GET method URLs in the application
+    """
+    get_endpoints = []
+    [
+        get_endpoints.append(route.path)
+        for route in app.routes
+        if "GET" in route.methods
+    ]
+    return get_endpoints[4:]
+
+
+@app.get("/get-endpoints", response_model=List[str])
+async def read_get_endpoints():
+    """
+    Lists all other GET method URLs
+    """
+    return get_all_get_endpoints()
