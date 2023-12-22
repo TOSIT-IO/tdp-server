89cf3a8d226baacdded21d21d394213b1d08a7d8
SteBaum <stephan.baum@edu.dsti.institute>
2023-12-22 17:48:28 +0100

    refactor: plandag schema

--- tdp_server/schemas/plan.py
+++ tdp_server/schemas/plan.py
@@ -1,8 +1,8 @@
-from datetime import datetime
 from pydantic import BaseModel
 from typing import Optional, List
 
-from tdp.core.models.state_enum import OperationStateEnum
+from tdp.core.models.state_enum import DeploymentStateEnum, OperationStateEnum
+from tdp.core.models.deployment_model import DeploymentTypeEnum
 from tdp.core.utils import BaseEnum
 
 
@@ -12,13 +12,20 @@ class PlanOptionsDag(BaseEnum):
     stop: Optional[bool] = False
 
 
-class PlanDag(BaseModel):
+class PlanOperations(BaseModel):
     deployment_id: int
     operation_order: int
     operation: str
     host: Optional[str]
     extra_vars: Optional[List[str]]
-    start_time: Optional[datetime]
-    end_time: datetime
     state: OperationStateEnum
     logs: Optional[str]
+    options: Optional[dict]
+
+
+class PlanDag(BaseModel):
+    deployment_id: int
+    state: Optional[DeploymentStateEnum]
+    deployment_type: Optional[DeploymentTypeEnum]
+    Operations: List[PlanOperations]
+    options: Optional[dict]
