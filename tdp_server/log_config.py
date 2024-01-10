38017e78c26eaada80de6979ef460b00aa47a2be
SteBaum <stephan.baum@edu.dsti.institute>
2024-01-10 18:34:41 +0100

    feat: added logger

--- tdp_server/log_config.py
+++ tdp_server/log_config.py
@@ -29,8 +29,8 @@ logging_config = {
 # create logger
 logger = logging.getLogger("tdp_server_logger")
 # 'application' code
-logger.debug("debug message")
-logger.info("info message")
-logger.warning("warn message")
-logger.error("error message")
-logger.critical("critical message")
+# logger.debug("debug message")
+# logger.info("info message")
+# logger.warning("warn message")
+# logger.error("error message")
+# logger.critical("critical message")
